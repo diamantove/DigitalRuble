652c8dbd286e2bfe83ad3aa87ef1af008235f71d
diamantove <diamantove@mail.ru>
2026-09-04 13:56:05 +0300

    feat: добавил CORS политику для фронтенда

--- backend/Api/DependencyInjection.cs
+++ backend/Api/DependencyInjection.cs
@@ -4,10 +4,23 @@ using Api.Exceptions.Handlers;
 
 namespace Api;
 
+
 public static class DependencyInjection
 {
+    private const string FrontendPolicyName = "FrontendPolicy";
+
     public static IServiceCollection AddApiServices(this IServiceCollection services)
     {
+        services.AddCors(options =>
+        {
+            options.AddPolicy(FrontendPolicyName, policy =>
+            {
+                policy.WithOrigins("http://localhost:5173")
+                      .AllowAnyHeader()
+                      .AllowAnyMethod();
+            });
+        });
+
         services
             .AddControllers()
             .AddJsonOptions(options =>
@@ -35,8 +48,7 @@ public static class DependencyInjection
         app.UseExceptionHandler();
         app.UseHttpsRedirection();
 
-        app.UseDefaultFiles();
-        app.UseStaticFiles();
+        app.UseCors(FrontendPolicyName);
 
         app.UseAuthorization();
 
